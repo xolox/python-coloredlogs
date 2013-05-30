fc1f643608165b13078993b9fcbf4bab3b8e459d
Peter Odding <peter@peterodding.com>
2013-05-30 21:52:52 +0200

    Change text styles (seems like an improvement to me)

--- coloredlogs.py
+++ coloredlogs.py
@@ -4,7 +4,7 @@
 Colored terminal output for Python's logging module.
 
 Author: Peter Odding <peter@peterodding.com>
-Last Change: May 16, 2013
+Last Change: May 30, 2013
 URL: https://pypi.python.org/pypi/coloredlogs
 
 The ColoredStreamHandler class enables colored terminal output for a logger
@@ -70,8 +70,12 @@ class ColoredStreamHandler(logging.StreamHandler):
         # Apply escape sequences to color the fields?
         name = self.wrap_color('blue', '%s[%s]' % (name, self.pid))
         timestamp = self.wrap_color('green', timestamp)
-        if severity in ('WARNING', 'ERROR', 'CRITICAL'):
-            message = self.wrap_color('red', message, bold=(severity in ('ERROR', 'CRITICAL')))
+        if severity == 'CRITICAL':
+            message = self.wrap_color('red', message, bold=True)
+        elif severity == 'ERROR':
+            message = self.wrap_color('red', message)
+        elif severity == 'WARNING':
+            message = self.wrap_color('yellow', message)
         elif severity == 'DEBUG':
             message = self.wrap_color('green', message)
         severity = self.wrap_color('black', severity, bold=True)
