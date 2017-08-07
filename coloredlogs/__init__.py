c446f3b20866bcf412f3ff83d09186208574be88
Peter Odding <peter@peterodding.com>
2017-08-07 22:48:13 +0200

    Release 7.3: Support for SUCCESS log level (fixes #27, see also xolox/python-verboselogs#4)

--- coloredlogs/__init__.py
+++ coloredlogs/__init__.py
@@ -199,7 +199,7 @@ WINDOWS = sys.platform.startswith('win')
 NEED_COLORAMA = WINDOWS
 
 # Semi-standard module versioning.
-__version__ = '7.2'
+__version__ = '7.3'
 
 DEFAULT_LOG_LEVEL = logging.INFO
 """The default log level for :mod:`coloredlogs` (:data:`logging.INFO`)."""
