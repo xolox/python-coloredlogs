b97b94c38ad50662b6c0c3c9de881c4c59c85f16
Peter Odding <peter@peterodding.com>
2018-01-05 04:22:24 +0100

    Release 8.0: Set default date format in formatTime()
    I decided to bump the major version number because this change is
    technically backwards incompatible, even though it concerns a minor
    detail (datefmt being set to its default value in the initializer).
    
    Then again, this change does improve compatibility with the behavior
    of the logging.Formatter class in Python's standard library,
    so there's that :-).

--- coloredlogs/__init__.py
+++ coloredlogs/__init__.py
@@ -199,7 +199,7 @@ WINDOWS = sys.platform.startswith('win')
 NEED_COLORAMA = WINDOWS
 
 # Semi-standard module versioning.
-__version__ = '7.3.3'
+__version__ = '8.0'
 
 DEFAULT_LOG_LEVEL = logging.INFO
 """The default log level for :mod:`coloredlogs` (:data:`logging.INFO`)."""
