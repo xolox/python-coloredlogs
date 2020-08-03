9c7d35de751992064d418f34ceef95d7101ef03f
Chris Ho <sudoerchris.ho@pm.me>
2020-08-03 18:20:07 +0800

    respect isatty parameter
    respect isatty parameter to force enable ANSI escape sequence
    fix issue #85

--- coloredlogs/__init__.py
+++ coloredlogs/__init__.py
@@ -430,7 +430,8 @@ def install(level=None, **kw):
             if on_windows():
                 use_colors = enable_ansi_support()
             # Disable ANSI escape sequences if 'stream' isn't connected to a terminal.
-            if use_colors or use_colors is None:
+            isatty = kw.get('isatty', False)
+            if (not isatty) and (use_colors or use_colors is None):
                 use_colors = terminal_supports_colors(stream)
         # Create a stream handler.
         handler = logging.StreamHandler(stream) if stream else StandardErrorHandler()
