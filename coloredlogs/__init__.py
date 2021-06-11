xolox/python-coloredlogs

commit 4f8c9aa8ad473f8dddbd37b3e8f0274d57e1ad73

Bug fix for issue #100 (disable colored text on output redirection)
Details: https://github.com/xolox/python-coloredlogs/issues/100

coloredlogs/__init__.py
@@ -1,7 +1,7 @@
 # Colored terminal output for Python's logging module.
 #
 # Author: Peter Odding <peter@peterodding.com>
-# Last Change: February 16, 2020
+# Last Change: December 8, 2020
 # URL: https://coloredlogs.readthedocs.io
 
 """
@@ -384,7 +384,7 @@ def install(level=None, **kw):
     """
     logger = kw.get('logger') or logging.getLogger()
     reconfigure = kw.get('reconfigure', True)
-    stream = kw.get('stream', None)
+    stream = kw.get('stream', sys.stderr)
     style = check_style(kw.get('style') or DEFAULT_FORMAT_STYLE)
     # Get the log level from an argument, environment variable or default and
     # convert the names of log levels to numbers to enable numeric comparison.

coloredlogs/tests.py
@@ -1,7 +1,7 @@
 # Automated tests for the `coloredlogs' package.
 #
 # Author: Peter Odding <peter@peterodding.com>
-# Last Change: February 16, 2020
+# Last Change: December 8, 2020
 # URL: https://coloredlogs.readthedocs.io
 
 """Automated tests for the `coloredlogs` package."""
@@ -18,7 +18,7 @@ import tempfile
 
 # External dependencies.
 from humanfriendly.compat import StringIO
-from humanfriendly.terminal import ANSI_COLOR_CODES, ansi_style, ansi_wrap
+from humanfriendly.terminal import ANSI_COLOR_CODES, ANSI_CSI, ansi_style, ansi_wrap
 from humanfriendly.testing import PatchedAttribute, PatchedItem, TestCase, retry
 from humanfriendly.text import format, random_string
 
@@ -388,6 +388,35 @@ class ColoredLogsTestCase(TestCase):
             assert text in last_line
             assert severity in last_line
             assert PLAIN_TEXT_PATTERN.match(last_line)
+
+    def test_auto_disable(self):
+        """
+        Make sure ANSI escape sequences are not emitted when logging output is being redirected.
+
+        This is a regression test for https://github.com/xolox/python-coloredlogs/issues/100.
+
+        It works as follows:
+
+        1. We mock an interactive terminal using 'capturer' to ensure that this
+           test works inside test drivers that capture output (like pytest).
+
+        2. We launch a subprocess (to ensure a clean process state) where
+           stderr is captured but stdout is not, emulating issue #100.
+
+        3. The output captured on stderr contained ANSI escape sequences after
+           this test was written and before the issue was fixed, so now this
+           serves as a regression test for issue #100.
+        """
+        with CaptureOutput():
+            interpreter = subprocess.Popen([
+                sys.executable, "-c", ";".join([
+                    "import coloredlogs, logging",
+                    "coloredlogs.install()",
+                    "logging.info('Hello world')",
+                ]),
+            ], stderr=subprocess.PIPE)
+            stdout, stderr = interpreter.communicate()
+            assert ANSI_CSI not in stderr.decode('UTF-8')
 
     def test_html_conversion(self):
         """Check the conversion from ANSI escape sequences to HTML."""

commit 8a2f2d102e84ec05980d543ecda51c18eac4eacd

Bug fix: Restore StandardErrorHandler functionality
The StandardErrorHandler class is responsible for dynamically resolving
(looking up the value of) sys.stderr for each logged message instead of
once when coloredlogs.install() is called.

This was unintentionally broken by changes in 4f8c9aa8ad4 which were
released as part of coloredlogs version 14.1.

This was brought to my attention by @hugovk here:
https://github.com/xolox/python-humanfriendly/pull/51

coloredlogs/__init__.py
@@ -1,7 +1,7 @@
 # Colored terminal output for Python's logging module.
 #
 # Author: Peter Odding <peter@peterodding.com>
-# Last Change: December 10, 2020
+# Last Change: June 11, 2021
 # URL: https://coloredlogs.readthedocs.io
 
 """
@@ -394,7 +394,7 @@ def install(level=None, **kw):
     """
     logger = kw.get('logger') or logging.getLogger()
     reconfigure = kw.get('reconfigure', True)
-    stream = kw.get('stream', sys.stderr)
+    stream = kw.get('stream') or sys.stderr
     style = check_style(kw.get('style') or DEFAULT_FORMAT_STYLE)
     # Get the log level from an argument, environment variable or default and
     # convert the names of log levels to numbers to enable numeric comparison.
@@ -453,7 +453,10 @@ def install(level=None, **kw):
         # Create a stream handler and make sure to preserve any filters
         # the current handler may have (if an existing handler is found).
         filters = handler.filters if handler else None
-        handler = logging.StreamHandler(stream) if stream else StandardErrorHandler()
+        if stream is sys.stderr:
+            handler = StandardErrorHandler()
+        else:
+            handler = logging.StreamHandler(stream)
         handler.setLevel(level)
         if filters:
             handler.filters = filters

coloredlogs/tests.py
@@ -1,7 +1,7 @@
 # Automated tests for the `coloredlogs' package.
 #
 # Author: Peter Odding <peter@peterodding.com>
-# Last Change: December 10, 2020
+# Last Change: June 11, 2021
 # URL: https://coloredlogs.readthedocs.io
 
 """Automated tests for the `coloredlogs` package."""
@@ -395,6 +395,22 @@ class ColoredLogsTestCase(TestCase):
             assert text in last_line
             assert severity in last_line
             assert PLAIN_TEXT_PATTERN.match(last_line)
+
+    def test_dynamic_stderr_lookup(self):
+        """Make sure coloredlogs.install() uses StandardErrorHandler when possible."""
+        coloredlogs.install()
+        # Redirect sys.stderr to a temporary buffer.
+        initial_stream = StringIO()
+        initial_text = "Which stream will receive this text?"
+        with PatchedAttribute(sys, 'stderr', initial_stream):
+            logging.info(initial_text)
+        assert initial_text in initial_stream.getvalue()
+        # Redirect sys.stderr again, to a different destination.
+        subsequent_stream = StringIO()
+        subsequent_text = "And which stream will receive this other text?"
+        with PatchedAttribute(sys, 'stderr', subsequent_stream):
+            logging.info(subsequent_text)
+        assert subsequent_text in subsequent_stream.getvalue()
 
     def test_force_enable(self):
         """Make sure ANSI escape sequences can be forced (bypassing auto-detection)."""